f34bdbed1919899b64650a69aa98b1cd2eb0bc70
Mennat Allah <manoshkamal316@gmail.com>
2023-05-25 20:56:01 +0300

    Shop

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import Footer from "./components/Footer";
 import NavBar from "./components/NavBar";
 import ProductCard from "./components/ProductCard";
+import Shop from "./pages/Shop";
 
 function App() {
   return (
     <>
       <NavBar />
-      <ProductCard />
+      {/* <ProductCard /> */}
+      <Shop />
       <Footer />
     </>
   );
